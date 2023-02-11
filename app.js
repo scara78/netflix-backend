eb7e40dae839f43be9b03d0465d75fa0e1ce6ea4
qolbudr <qolbu.dzikru30@gmail.com>
2023-02-11 10:48:57 +0700

    complete be

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const axios = require("axios")
+const fs = require('fs')
+const app = express()
+const port = 3000
+const path = require('path')
+const utils = require('./utils.js')
+
+app.get("/api/home", async (req, res) => {
+	const data = await utils.getHome();
+	res.send(data)
+})
+
+app.get("/api/detail", async (req, res) => {
+	const tmdb = req.query.tmdb
+	const type = req.query.type
+	const data = await utils.getDetail(tmdb, type);
+	res.send(data)
+})
+
+app.get("/api/episode", async (req, res) => {
+	const tmdb = req.query.tmdb
+	const season = req.query.season
+	const data = await utils.getEpisode(tmdb, season);
+	res.send(data)
+})
+
+app.get("/api/search", async (req, res) => {
+	try {
+		const title = req.query.title
+		const page = req.query.page
+		const data = await utils.getSearch(title, page);
+		res.send(data)
+	} catch {
+		return res.send([])
+	}
+})
+
+app.get("/api/genre", async (req, res) => {
+	try {
+		const genre = req.query.genre
+		const page = req.query.page
+		const data = await utils.getGenre(genre, page);
+		res.send(data)
+	} catch {
+		return res.send([])
+	}
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+module.exports = app;
---
+++ utils.js
@@ -0,0 +1,147 @@
+const axios = require("axios")
+const apiKey = 'cad7722e1ca44bd5f1ea46b59c8d54c8'
+const baseURL = 'https://api.themoviedb.org/3'
+const gDrive = 'https://api.gdriveplayer.us'
+const genreList = require("./data/genre.json");
+const banner = require("./data/banner.json");
+
+const toTmdb = async (movie) => {
+	let result = {}
+	let genre = []
+	let response = await axios.get(`${baseURL}/find/${movie.imdb}?api_key=${apiKey}&external_source=imdb_id`)
+	let data = response.data
+	try {
+		result = data.movie_results[0]
+		result.genre_ids.forEach((item) => {
+			genre.push(genreList[item])
+		})
+		result['imdb'] = movie.imdb
+		result['runtime'] = movie.runtime
+		result['genre'] = genre;
+		result['season'] = null;
+		return result
+	} catch {
+		try {
+			result = data.tv_results[0]
+			result.genre_ids.forEach((item) => {
+				genre.push(genreList[item])
+			})
+			result['genre'] = genre;
+			result['imdb'] = movie.imdb
+			result['runtime'] = movie.runtime
+			result['genre'] = genre;
+			result['season'] = parseInt(movie.detail.substring(movie.detail.lastIndexOf("/")).replaceAll('/season', ''))
+			return result	
+		} catch (e) {
+			
+		}
+	}
+}
+
+const getHome = async () => {
+	const response = {}
+	let data
+	let tmdb
+	let promise = []
+
+	//banner
+	data = banner
+	tmdb = await toTmdb(data)
+	response['banner'] = tmdb
+
+	//action
+	const action = await axios.get(`${gDrive}/v1/movie/search?genre=action`)
+	data = action.data
+	data.forEach((item) => {
+		promise.push(toTmdb(item))
+	})
+	response['action'] = await Promise.all(promise)
+	response['action'] = response['action'].filter((item) => item)
+	promise = []
+
+	//romance
+	const romance = await axios.get(`${gDrive}/v1/movie/search?genre=romance`)
+	data = romance.data
+	data.forEach((item) => {
+		promise.push(toTmdb(item))
+	})
+	response['romance'] = await Promise.all(promise)
+	response['romance'] = response['romance'].filter((item) => item)
+	promise = []
+
+	// series
+	const series = await axios.get(`${gDrive}/v2/series/newest`)
+	data = series.data
+	data.forEach((item) => {
+		promise.push(toTmdb(item))
+	})
+
+	response['series'] = await Promise.all(promise)
+	response['series'] = response['series'].filter((item) => item)
+	return response
+}
+
+const getDetail = async (tmdb, type) => {
+	if(type == 'movie') {
+		let response = await axios.get(`${baseURL}/movie/${tmdb}?api_key=${apiKey}&append_to_response=videos,images`)
+		return response.data
+	} else {
+		let response = await axios.get(`${baseURL}/tv/${tmdb}?api_key=${apiKey}&append_to_response=videos,images`)
+		return response.data
+	}
+}
+
+const getEpisode = async (tmdb, season) => {	
+	let response = await axios.get(`${baseURL}/tv/${tmdb}/season/${season}?api_key=${apiKey}`)
+	return response.data
+}
+
+const getSearch = async (title, page) => {
+	let result = []
+	const promise = []
+
+	const resMovie = await axios.get(`${gDrive}/v1/movie/search?title=${title}&limit=5&page=${page}`)
+	if(resMovie.data != null) {
+		resMovie.data.forEach((item) => {
+			promise.push(toTmdb(item))
+		})
+	}
+
+	const resSeries = await axios.get(`${gDrive}/v2/series/search?title=${title}&limit=5&page=${page}`)
+	if(resSeries.data != null) {
+		resSeries.data.forEach((item) => {
+			promise.push(toTmdb(item))
+		})
+	}
+
+	result = await Promise.all(promise)
+	result = result.filter((item) => item);
+
+	return result
+}
+
+const getGenre = async (genre, page) => {
+	let result = []
+	const promise = []
+
+	const resMovie = await axios.get(`${gDrive}/v1/movie/search?genre=${genre}&limit=5&page=${page}`)
+	if(resMovie.data != null) {
+		resMovie.data.forEach((item) => {
+			promise.push(toTmdb(item))
+		})
+	}
+
+	const resSeries = await axios.get(`${gDrive}/v2/series/search?genre=${genre}&limit=5&page=${page}`)
+	if(resSeries.data != null) {
+		resSeries.data.forEach((item) => {
+			promise.push(toTmdb(item))
+		})
+	}
+
+	result = await Promise.all(promise)
+	result = result.filter((item) => item);
+
+	return result
+}
+
+module.exports = { getHome, getDetail, getEpisode, getSearch, getGenre }
